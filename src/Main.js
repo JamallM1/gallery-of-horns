774f95cd38e0ab7fe522dff39a54a8b7b2900dbb
Jamall Malik <mimsjamall79@gmail.com>
2022-06-15 23:36:14 -0400

    lab03

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import data from './data.json'
+
 
 class Main extends React.Component {
   render() {
-    let beasts = data.map((beast, index) => {
+    let beasts = this.props.data.map((beast, index) => {
       
       return <HornedBeast
         title={beast.title}
         imageurl={beast.image_url}
         description={beast.description}
+        openModal ={this.props.openModal}
+        beast= {beast} 
         key={index}
       />
     })
